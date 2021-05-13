3df3d8cb96110ae1bc7f93da0c78a347f8d2965c
imamura-slab <atuuyarumami0704@yahoo.co.jp>
2021-05-13 14:26:26 +0900

    change display message

--- day01_c/hello.c
+++ day01_c/hello.c
@@ -20,7 +20,8 @@ typedef struct {
 
 EFI_STATUS EfiMain(EFI_HANDLE        ImageHandle,
                    EFI_SYSTEM_TABLE  *SystemTable) {
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
+  //SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
+  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, imamura-slab!!\n");
   while (1);
   return 0;
 }
